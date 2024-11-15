0e7e69fa1a379bb659565b16afa8ef571de93843
rizahmeds <rizahmed.in@gmail.com>
2024-11-15 19:43:36 +0530

    feat: initial project setup

---
+++ exchange/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Currency(models.ProtectedModel):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=20, db_index=True)
+    symbol = models.CharField(max_length=10)
+
+
+class CurrencyExchangeRate(models.Model):
+    source_currency = models.ForeignKey(Currency,related_name='exchanges', on_delete=models.CASCADE)
+    exchanged_currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    valuation_date = models.DateField(db_index=True)
+    rate_value = models.DecimalField(db_index=True, decimal_places=6, max_digits=18)
